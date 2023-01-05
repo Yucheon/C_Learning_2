6b7b6f65005d8eee1ac8efed4e3ae76f336a2e6d
Beom <gbj1025@gmail.com>
2023-01-05 17:37:52 +0900

    01.05 study

--- Chpater5_09/Chapter05_10.cpp
+++ Chpater5_09/Chapter05_10.cpp
@@ -2,78 +2,39 @@
 
 using namespace std;
 
-int getInt()		//do while이랑 for 사용하기
+int getInt()
 {
-	while (true)
+	cout << "Enter an integer number : ";
+	int x;
+	cin >> x;
+	if (cin.fail())
 	{
-		cout << "Please enter number" << endl;
-		int x;
-		cin >> x;
-
-		if (std::cin.fail())
-		{
-			std::cin.clear();					//싹지움
-			std::cin.ignore(32767, '\n');		//버퍼를 비워줌
-			cout << "정상적인 수가 아닙니다." << endl;
-		}
-
-		else
-		{
-			std::cin.ignore(32767, '\n');
-			return x;
-		}
+		cin.clear();
+		cin.ignore(32767, '\n');
+		cout << "Invaild number, please try again" << endl;
+	}
+	else
+	{
+		std::cin.ignore(32767, '\n');
+		return x;
 	}
 }
 
 char getOperator()
 {
-	while (true)
-	{
-
-	cout << "Please enter Operator" << endl;
+	cout << "Enter an operator (+,-) : ";
 	char op;
 	cin >> op;
-	std::cin.ignore(32767, '\n');		//버퍼를 비워줌
-
-	if (op == '+' || op == '-'|| op == '*' ||op == '/')
-		return op;
-	else
-		cout << "똑바로 넣으세요" << endl;
-	}
-
+	return op;
 }
-
-void printResult(int x, char op, int y)		//case 사용하기
+void printResult(int x, char op, int y)
 {
-	switch (op)
+	if (op == '+') cout << x + y << endl;
+	else if (op == '-') cout << x - y << endl;
+	else
 	{
-	case '+':
-		{
-		cout << x + y << endl;
-		}break;
-	case '-':
-	{
-		cout << x - y << endl;
-	}break;
-	case '*':
-	{
-		cout << x * y << endl;
-	}break;
-	case '/':
-	{
-		cout << x / y << endl;
-	}break;
-	default:
-	{
-		cout << "잘못 입력했어" << endl;
-	}break;
+		cout << "Invalid operator" << endl;
 	}
-	//if (op == '+')cout << x + y << endl;
-	//else if (op == '-')cout << x - y << endl;
-	//else
-	//{
-	//	cout << "Invalid operator" << endl;
-	//}
 }
 
 int main()
@@ -83,5 +44,6 @@ int main()
 	int y = getInt();
 
 	printResult(x, op, y);
+
 	return 0;
 }
