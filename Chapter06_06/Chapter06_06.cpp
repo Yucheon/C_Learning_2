e2bb47863ee4247c0c41b4ed585701d96d444d97
Beom <gbj1025@gmail.com>
2023-01-18 21:16:05 +0900

    01/18 공부

--- Chapter06_06/Chapter06_06.cpp
+++ Chapter06_06/Chapter06_06.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
-	char myString[255];
+	char source[] = "Copy this!";	//11자리
+	char dest[5];					//11자리 보다 메모리를 더 적게 넣게 되면
+	strcpy(dest, source);			//런타임 에러 발생!!
 
-	//cin >> myString;		//HelloMyFriend 입력시
-	cin.getline(myString, 255);
+	cout << source << endl;
+	cout << dest << endl;
 
-	cout << myString;
 	return 0;
 }
